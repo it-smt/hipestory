52eb37d6c901929029b7cc9273ee844dbea0a703
it-smt <smt_razrab@mail.ru>
2023-10-13 23:00:57 +0500

    admin updated

--- main/admin.py
+++ main/admin.py
@@ -5,10 +5,11 @@ from main.models import Story, StoryComment, Task, TaskCommentAnswer
 
 @admin.register(Story)
 class StoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'created', 'updated', 'is_active', 'status')
+    list_display = ('title', 'slug', 'author', 'created', 'updated', 'is_active', 'status')
     list_filter = ('created', 'updated', 'is_active', 'status')
-    fields = (('title', 'slug'), 'body', 'is_active', 'status')
+    fields = (('title', 'slug'), 'author', 'body', 'is_active', 'status')
     prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
 
 
 @admin.register(StoryComment)
